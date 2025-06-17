7fa772119bf8a3d83a512b25f3d094548c827785
Jeffry Steve <jeffrysteve2005@gmail.com>
2025-06-17 08:58:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import json
 import os
+from flask_cors import CORS
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 app = Flask(__name__)
 CORS(app)
